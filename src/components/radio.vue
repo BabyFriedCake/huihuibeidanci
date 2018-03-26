b0480c1cdac63b9a4fd87dd82d9b28eabab7af90
BabyFriedCake <1079047055@qq.com>
2018-03-26 10:33:23 +0800

    单选

--- src/components/radio.vue
+++ src/components/radio.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>
+  <div id="Radio">
       <input type="radio">
   </div>
 </template>
+<script>
+export default {
+  name:'Radio',
+
+}
+</script>
+<style lang="less" scoped>
+@import '../assets/css/variables.less';
+input:checked{
+  
+}
+</style>
+
--- src/page/CreatSelect.vue
+++ src/page/CreatSelect.vue
@@ -1,14 +1,28 @@
 <template>
   <div id="CreatSelect">
-      <div>
-        <p class="text-lg">请选择考试时间</p>
-        <p class="text-lg">CET考试时间定于每年6月和12月的第三个星期六</p>
-        <p class="text-md">2017年</p>
-        <p class="text-lg">选择其他的考试时间</p>
-        <p>预留备考时间</p>
-        <p>考试的多少天计划</p>
+      <div class="padding-lg">
+        <p class="text-size-lg">请选择考试时间</p>
+        <div class="padding-sm"></div>
+        <div class="text-size-lg-note">CET考试时间定于每年6月和12月的第三个星期六</div>
+        <div class="padding-lg"></div>
+        <div class="text-size-md">2017年</div>
+        <div class="text-size-lg">选择其他的考试时间</div>
+        <div class="padding-lg"></div>
+        <div class="radio-lg">
+          <ul>
+            <li class="active">
+              <div class="context">6月17日</div>
+            </li>
+            <li class="no-active">
+              <div class="context">12月16日</div>
+            </li>
+          </ul>
+          <div class="cf"></div>
+        </div>
+        <div>预留备考时间</div>
+        <div>考试的多少天计划</div>
       </div>
-      <nav-btn @click="goCreatSelect" val="开始吧" color="green" class="bottomBtn"></nav-btn>
+      <nav-btn @click="goCreatSelect" val="开始吧" color="orange"></nav-btn>
   </div>
 </template>
 <script>
@@ -27,5 +41,37 @@ export default {
 </script>
 <style lang="less" scoped>
 @import '../assets/css/variables.less';
-body{background: url(../assets/images/tartan.png) repeat}
+#CreatSelect{
+  background: url(../assets/images/tartan.png) repeat;
+  height: 1000px;
+}
+.radio-lg{
+  li{
+    float: left;
+    width: 40%;
+    height: 100px;
+    padding:5px;
+  }
+  .active{
+    border:1px solid @green;
+    div{
+      background-color: @green;
+      height: 100px;
+      line-height: 100px;
+      text-align: center;
+      color: @white;
+    }
+  }
+  .no-active{
+    border:0;
+    div{
+      background-color: #eee;
+      height: 100px;
+      line-height: 100px;
+      text-align: center;
+      color: #333;
+      border:1px solid #d3d3d3
+    }
+  }
+}
 </style>
